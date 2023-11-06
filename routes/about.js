fd92b34b4286fea54e39354bed95b9b7372dc296
Leon <letconex@gmail.com>
2023-11-06 13:35:38 +0200

    Update about.js

--- routes/about.js
+++ routes/about.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// res.send('Get req to root');
 router.get('/', function (req, res, next) {
     res.render('about', { title: 'E-Factura'});
 });
